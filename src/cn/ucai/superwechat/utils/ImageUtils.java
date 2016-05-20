5ea73fae3f768e9f2bde7888926dabea25b6e473
huwei <530505193@qq.com>
2016-05-20 11:11:28 +0800

    在Utils包中的ImageUtils类中添加getAvatarPath方法，并在layout中添加popu_show_avatar

--- src/cn/ucai/superwechat/utils/ImageUtils.java
+++ src/cn/ucai/superwechat/utils/ImageUtils.java
@@ -13,8 +13,16 @@
  */
 package cn.ucai.superwechat.utils;
 
+import android.content.Context;
+import android.os.Environment;
+
 import com.easemob.util.EMLog;
 import com.easemob.util.PathUtil;
+
+
+
+import java.io.File;
+
 
 public class ImageUtils {
 //	public static String getThumbnailImagePath(String imagePath) {
@@ -41,6 +49,13 @@ public class ImageUtils {
         EMLog.d("msg", "thum image path:" + path);
         return path;
     }
-	
-	
+
+	public static String getAvatarPath(Context context, String path) {
+		File dir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+		File folder = new File(dir, path);
+		if (!folder.exists()) {
+			folder.mkdir();
+		}
+		return folder.getAbsolutePath();
+	}
 }
